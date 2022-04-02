14d903d71759ab2b53172e5b39ef73feec84b4f5
Tuan Anh Le <anh.le@myob.com>
2022-04-03 06:43:16 +1000

    feat: create Faucet contract

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -22,3 +22,16 @@ contract mortal is owner {
         selfdestruct(owner);
     }
 }
+
+contract Faucet is mortal {
+    // Give out ether to anyone who asks
+    function withdraw(uint withdraw_amount) public {
+        // Limit withdrawal amount
+        require(withdraw_amount <= 0.1 ether);
+        // Send the amount to the address that requested it
+        payable(msg.sender).transfer(withdraw_amount);
+    }
+
+    // Accept any incoming amount
+    receive() external payable {}
+}
